fd103a3475c985b87dafb6e969517a9c94dd1a2e
Mile <mwp@uade.edu.ar>
2026-04-11 19:56:09 -0300

    Agrego carpeta Interfaces con sus conexiones en cada clase

---
+++ Sonrisa Feliz/src/Interfaces/IPaciente.java
@@ -0,0 +1,27 @@
+package Interfaces;
+
+import modelo.Domicilio;
+import java.time.LocalDate;
+
+public interface IPaciente {
+    Long getId();
+    void setId(Long id);
+
+    String getNombre();
+    void setNombre(String nombre);
+
+    String getApellido();
+    void setApellido(String apellido);
+
+    String getDni();
+    void setDni(String dni);
+
+    String getEmail();
+    void setEmail(String email);
+
+    LocalDate getFechaIngreso();
+    void setFechaIngreso(LocalDate fechaIngreso);
+
+    Domicilio getDomicilio();
+    void setDomicilio(Domicilio domicilio);
+}

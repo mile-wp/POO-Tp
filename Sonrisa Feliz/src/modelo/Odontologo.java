d54a9017d9cb5d585e6abbe41536e185655a7f12
Mile <mwp@uade.edu.ar>
2026-04-11 19:58:34 -0300

    Agrego conexiones en cada clase

--- Sonrisa Feliz/src/modelo/Odontologo.java
+++ Sonrisa Feliz/src/modelo/Odontologo.java
@@ -1,15 +1,73 @@
 package modelo;
 
-public class Odontologo {
+import Interfaces.IOdontologo;
+
+public class Odontologo implements IOdontologo {
     private Long id;
     private String nombre;
     private String apellido;
     private String matricula;
 
+    //Creamos el constructor
+
     public Odontologo(Long id, String nombre, String apellido, String matricula) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.matricula = matricula;
+    }
+
+    //Creamos getters y setters
+
+    @Override
+    public Long getid() {
+        return 0L;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getnombre() {
+        return "";
+    }
+
+    @Override
+    public void setnombre(String nombre) {
+
+    }
+
+    @Override
+    public String getapellido() {
+        return "";
+    }
+
+    @Override
+    public void setapellido(String apellido) {
+
+    }
+
+    @Override
+    public String getmatricula() {
+        return "";
+    }
+
+    @Override
+    public void setmatricula(String matricula) {
+
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
 }
--- Sonrisa Feliz/src/modelo/Paciente.java
+++ Sonrisa Feliz/src/modelo/Paciente.java
@@ -1,8 +1,10 @@
 package modelo;
 
+import Interfaces.IPaciente;
+
 import java.time.LocalDate;
 
-public class Paciente {
+public class Paciente implements IPaciente {
     private Long id;
     private String nombre;
     private String apellido;
@@ -21,6 +23,8 @@ public class Paciente {
         this.domicilio = domicilio;
     }
 
+    //Agregamos constructor
+
     public void mostrarPaciente() {
         System.out.println("Id: "+ id);
         System.out.println("Nombre completo: "+ nombre +apellido);
@@ -28,5 +32,79 @@ public class Paciente {
         System.out.println("Correo electrónico: "+ email);
         System.out.println("Fecha de ingreso: "+ fechaIngreso);
         System.out.println("Domicilio: "+ domicilio);;
+    }
+
+
+    //Agregamos getters y setters
+
+
+    @Override
+    public Long getId() {
+        return 0L;
+    }
+
+    @Override
+    public void setId(Long id) {
+
+    }
+
+    @Override
+    public String getNombre() {
+        return "";
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+
+    }
+
+    @Override
+    public String getApellido() {
+        return "";
+    }
+
+    @Override
+    public void setApellido(String apellido) {
+
+    }
+
+    @Override
+    public String getDni() {
+        return "";
+    }
+
+    @Override
+    public void setDni(String dni) {
+
+    }
+
+    @Override
+    public String getEmail() {
+        return "";
+    }
+
+    @Override
+    public void setEmail(String email) {
+
+    }
+
+    @Override
+    public LocalDate getFechaIngreso() {
+        return null;
+    }
+
+    @Override
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+
+    }
+
+    @Override
+    public Domicilio getDomicilio() {
+        return null;
+    }
+
+    @Override
+    public void setDomicilio(Domicilio domicilio) {
+
     }
 }
